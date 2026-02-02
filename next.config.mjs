ac0fb11b51f640016c6efa4447e8ab1cbffa59c8
vabr0002 <143542161+vabr0002@users.noreply.github.com>
2026-02-02 14:35:46 +0100

    Setup Node 20 + output: export for Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', // <-- her fortæller vi Next.js at det skal lave statisk export
+}
 
 export default nextConfig;
